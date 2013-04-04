3da541fff6e88ed08066a293a6f26bab660d4449
zdxerr <zdxerr@gmail.com>
2013-04-04 12:05:29 +0200

    Fixed crtita import.

--- crtita.py
+++ crtita.py
@@ -1,26 +1,33 @@
 # -*- coding: utf-8 -*-
 
-import os
 import cPickle as pickle
 from pprint import pprint
 
-path = "E:\\result_analysis"
-files = ["crtita_result"]
+from result import ResultFile, Sequence
 
-def load(file_path):
-    with open(file_path, "r") as result_file:
-        result = pickle.load(result_file)
-    return result
+
+class CRTITAResultFile(ResultFile):
+    def parse(self):
+        with open(self.path, 'r') as result_file:
+            self.__result = pickle.load(result_file)
+
+        for key in self.__result:
+            print key
+
+        pprint(self.__result['EnvInfo'])
+
+        # pprint(self.__result['TestResult'])
+
 
 if __name__ == '__main__':
     result_path = 'R:\\PE\\Testdata\\CRTI-Test\\ImplSW_RLS_2013-A\\' \
                   'RTIFlexRay\\Res\\INT13\\T_01_RFT\\TS4\\Result\\' \
                   'crtita_result'
-    result = RTITEResultFile(result_path)
+    result = CRTITAResultFile(result_path)
     print result
-    print result.time
-    print result.description
+    # print result.time
+    # print result.description
 
     # pprint(result.environment)
-    pprint(result.run)
-    pprint(result.sequences)
+    # pprint(result.run)
+    # pprint(result.sequences)
