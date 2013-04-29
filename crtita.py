4085c05d0ba9aff5d301ad1b3ed612829055d62c
zdxerr <zdxerr@gmail.com>
2013-04-29 14:12:55 +0200

    Fixed sequence start/end.

--- crtita.py
+++ crtita.py
@@ -2,6 +2,7 @@
 
 import cPickle as pickle
 from pprint import pprint
+from datetime import timedelta
 
 import dateutil.parser
 
@@ -14,11 +15,12 @@ class CRTITAResult(ResultFile):
             self.__result = pickle.load(result_file)
 
         self.tags.insert(2, "CRTITA")
+        self.tags.pop()
 
-        for key in self.__result:
-            print key
+        # for key in self.__result:
+        #     print key
 
-        # pprint(self.__result['TestResult'])
+        # pprint(self.__result['EnvInfo'])
         # for r in self.__result['TestResult']:
         #     print r
         self.__parse_environment()
@@ -30,13 +32,15 @@ class CRTITAResult(ResultFile):
         env = self.__result['EnvInfo']
 
         self.os = env['6Operating system']
+        self.pc = "N/A"
+        self.platform = "N/A"
 
     def __parse_run(self):
         self.run = run = {}
         summary = self.__result['Summary']
 
         try:
-            self.description = self.__td["teparam"][0][0][0][0]["comment"][0]
+            self.description = self.result['TestrunConfig']['TestDescription']
         except:
             pass
 
@@ -50,38 +54,46 @@ class CRTITAResult(ResultFile):
             s = CRTITASequence(name, result[0]['status'])
             sequences.append(s)
             s.comment = result[0]['comment']
-            print s, type(result), len(result), result[0]['status'], result[0]['exitStatus'], result[0]['execTime']
-            for l in result:
-                print ' ', '.'*8
-                for k in l:
-                    print '  ', k
+            # print s, type(result), len(result), result[0]['status'], result[0]['exitStatus'], result[0]['execTime']
+            # for l in result:
+            #     print ' ', '.'*8
+            #     for k in l:
+            #         print '  ', k
+
+            # print '#'*8, result[0]['execTime']
+            dt = dateutil.parser.parse(result[0]['execTime'], fuzzy=True)
+            td = (timedelta(days=1)
+                  if dt.time() < self.start.time()
+                  else timedelta())
+
+            d = (self.start + td).date()
+
+            s.start = dt.replace(year=d.year, month=d.month, day=d.day)
+            s.end = s.start + timedelta(
+                seconds=float(result[0]['testDuration'].split(' ', 1)[0]))
 
             s.log = []
-
-
-
             for tc in result[0]['tcList']:
-                for k in tc:
-                    print k
                 s.log.append(
                     {
                         'id': tc['tcNum'],
                         'status': tc['status'],
-                        'message': tc['tcName']
+                        'message': tc['tcName'].strip(),
+                        'name': tc['tcName'].strip()
                     }
                 )
-                for ud in tc['userData']:
-                    # for k, v in ud.items():
-                    #     s.log.append(
-                    #         {
-                    #             'id': tc['tcNum'],
-                    #             'status': tc['status'],
-                    #             'message': "{} -> {}".format(k, v)
-                    #         }
-                    #     )
-                    s.log[-1]['message'] += "\n" + "\n".join(
-                        "{} -> {}".format(k, v) for k, v in ud.items())
-            break
+                if tc['userData']:
+                    for ud in tc['userData']:
+                        # for k, v in ud.items():
+                        #     s.log.append(
+                        #         {
+                        #             'id': tc['tcNum'],
+                        #             'status': tc['status'],
+                        #             'message': "{} -> {}".format(k, v)
+                        #         }
+                        #     )
+                        s.log[-1]['message'] += "\n" + "\n".join(
+                            "{} -> {}".format(k, v) for k, v in ud.items())
 
 
 class CRTITASequence(Sequence):
@@ -102,6 +114,9 @@ if __name__ == '__main__':
     result = CRTITAResult(result_path)
     print result
     pprint(result.tags)
+
+
+
     print result.start, result.end, result.end - result.start
 
     for sequence in result.sequences:
